ea00308642861e32294dbd37b74ec4af482ab751
NastyaMarkovets <anastasia.markovets@mail.ru>
2019-11-14 19:12:00 +0300

    Create base project

---
+++ JogTrackerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JogTrackerApp
+//
+//  Created by Anastasia Markovets on 11/13/19.
+//  Copyright © 2019 Lonely Tree Std. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

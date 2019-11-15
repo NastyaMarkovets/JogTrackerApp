52a38ea0e987f18d17ed9c971100bafa8da07ab3
NastyaMarkovets <anastasia.markovets@mail.ru>
2019-11-15 20:47:20 +0300

    Complete base navigation bar

--- JogTrackerApp/Presentation/Controllers/BaseViewController.swift
+++ JogTrackerApp/Presentation/Controllers/BaseViewController.swift
@@ -11,38 +11,86 @@ import SnapKit
 
 class BaseViewController: UIViewController {
     
+    private enum Dimensions {
+        static let navigationBarHeight: CGFloat = 77
+        static let contentSpacing: CGFloat = 20
+        static let sideInset: CGFloat = 25
+    }
+    
+    private let baseNavigationBarView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.Base.appleGreen
+        return view
+    }()
+    
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.tintColor = .white
+        return imageView
+    }()
+    
+    private let rightButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "menu"), for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(didTapedMenu), for: .touchUpInside)
+        return button
+    }()
+    
+    enum RightButtonType {
+        case menu
+        case cancel
+    }
+    var rightButtonType: RightButtonType = .menu
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.Base.backgroundColor
-        addLogo()
-        setupBaseNavigationBar()
+        self.navigationController?.isNavigationBarHidden = true
+        addSubviews()
+        setupConstraints()
     }
     
-    func setupBaseNavigationBar() {
-        navigationController?.navigationBar.isTranslucent = false
-        navigationController?.navigationBar.barTintColor = UIColor.Base.appleGreen
-        navigationController?.navigationBar.tintColor = .white
-        let menuIcon = UIImage(named: "menu")
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: menuIcon, style: .plain, target: self, action: #selector(didTapedMenu))
+    private func addSubviews() {
+        view.addSubview(baseNavigationBarView)
+        baseNavigationBarView.addSubview(logoImageView)
+        baseNavigationBarView.addSubview(rightButton)
+    }
+    
+    private func setupConstraints() {
+        baseNavigationBarView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(Dimensions.navigationBarHeight + UIApplication.shared.statusBarFrame.height)
+        }
+        
+        logoImageView.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(Dimensions.contentSpacing)
+            $0.leading.equalToSuperview().inset(Dimensions.sideInset)
+        }
+        
+        rightButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(Dimensions.sideInset)
+            $0.bottom.equalToSuperview().inset(Dimensions.sideInset)
+        }
+    }
+    
+    func setupMenuNavigationBar() {
+        baseNavigationBarView.backgroundColor = .white
+        rightButton.setImage(UIImage(named: "cancel"), for: .normal)
+        rightButton.tintColor = UIColor.Base.greyish
+        logoImageView.tintColor = UIColor.Base.appleGreen
+        rightButtonType = .cancel
     }
     
     @objc private func didTapedMenu() {
-        let menuViewController = MenuViewController()
-        navigationController?.pushViewController(menuViewController, animated: true)
+        switch rightButtonType {
+        case .menu:
+            let menuViewController = MenuViewController()
+            navigationController?.pushViewController(menuViewController, animated: true)
+        case .cancel:
+            navigationController?.popViewController(animated: true)
+        }
     }
-    
-    func addLogo() {
-        let logoIcon = UIImage(named: "logo")
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: logoIcon, style: .plain, target: nil, action: nil)
-    }
-    
-    func whiteNavigationBarSetup() {
-        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        navigationController?.navigationBar.shadowImage = UIImage()
-        navigationController?.navigationBar.isTranslucent = true
-        navigationController?.view.backgroundColor = .clear
 
-        navigationController?.navigationBar.tintColor = UIColor.Base.appleGreen
-    }
-    
 }
--- JogTrackerApp/Presentation/Controllers/MenuViewController.swift
+++ JogTrackerApp/Presentation/Controllers/MenuViewController.swift
@@ -13,24 +13,7 @@ class MenuViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.Base.backgroundColor
-        
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        whiteNavigationBarSetup()
-        let cancelIcon = UIImage(named: "cancel")
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: cancelIcon, style: .plain, target: self, action: #selector(cancelTapped))
-        navigationItem.rightBarButtonItem?.tintColor = UIColor.Base.greyish
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        setupBaseNavigationBar()
-    }
-    
-    @objc private func cancelTapped() {
-        navigationController?.popViewController(animated: true)
+        setupMenuNavigationBar()
     }
 
 }
